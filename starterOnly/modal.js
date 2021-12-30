679189e1d793b5f9948fc496a2b4643bf64b245e
Hugo Olekhnovitch <hugo@olek.fr>
2021-12-30 09:32:56 +0100

    close modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,23 +1,27 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
+  var x = document.getElementById('myTopnav');
+  if (x.className === 'topnav') {
+    x.className += ' responsive';
   } else {
-    x.className = "topnav";
+    x.className = 'topnav';
   }
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+const modalbg = document.querySelector('.bground');
+const modalBtn = document.querySelectorAll('.modal-btn');
+const formData = document.querySelectorAll('.formData');
+const closeModalBtn = document.querySelectorAll('span.close');
 
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalBtn.forEach((btn) => btn.addEventListener('click', launchModal));
+closeModalBtn.forEach((btn) => btn.addEventListener('click', closeModalForm));
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+  modalbg.style.display = 'block';
 }
 
-
+function closeModalForm() {
+  modalbg.style.display = 'none';
+}
