148901ac1e61b9d8ab279ea4f3478c3cffb6052e
Hugo Olekhnovitch <hugo@olek.fr>
2022-01-06 12:38:21 +0100

    correction messages erreurs

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,20 +1,31 @@
 // DOM elements
 const form = document.querySelector('form');
 const input_text = document.querySelectorAll('input[type=text]');
+const input_email = document.querySelector('input[type=email]');
+const input_number = document.querySelector('input[type=number]');
+const checkbox_cu = document.querySelector('#checkbox1');
 
 form.addEventListener('submit', validate);
 input_text.forEach((input) => input.addEventListener('blur', validateText));
+input_email.addEventListener('blur', validateEmail);
+input_number.addEventListener('blur', validateNumber);
 
 // Submit form
 function validate(event) {
     event.preventDefault();
+    var errors = false;
+
+    if (!checkbox_cu.checked) {
+        showErrorMessage(checkbox_cu, 'Vous devez vérifier que vous acceptez les termes et conditions.');
+    }
+
     console.log('Merci pour votre inscription !');
 }
 
 function validateText(event) {
     console.log('test');
     var input_value = event.target.value.trim();
-    if (input_value < 2) {
+    if (input_value.length < 2) {
         showErrorMessage(event.target, 'Veuillez entrer 2 caractères ou plus pour le champ du nom.');
     } else {
         hideErrorMessage(event.target);
@@ -23,24 +34,32 @@ function validateText(event) {
 
 function validateEmail(event) {
     // Si juste
-    hideErrorMessage(event.target);
-    // Si error
-    showErrorMessage(event.target, 'erreur');
+    if (event.target.value.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
+        hideErrorMessage(event.target);
+    } else {
+        // Si error
+        showErrorMessage(event.target, 'Veuillez entrer une adresse email valide.');
+    }
+}
+
+function validateNumber(event) {
+    if (event.target.value != '' && Number.isInteger(event.target.value)) {
+        hideErrorMessage(event.target);
+    } else {
+        showErrorMessage(event.target, 'Veuillez entrer un chiffre entier valide.');
+    }
 }
 
 function showErrorMessage(input, message) {
     hideErrorMessage(input);
-    // ajout du span
-    var span = document.createElement('span');
-    span.append(message);
-    input.parentElement.append(span);
-    // ajout class
-    input.parentElement.classList.add('error');
+    // error
+    input.parentElement.setAttribute('data-error', message);
+    input.parentElement.setAttribute('data-error-visible', true);
 }
 
 function hideErrorMessage(input) {
-    var span = input.parentElement.querySelector('span');
-    if (span) {
-        span.remove();
+    if (input.parentElement.hasAttribute('data-error')) {
+        input.parentElement.removeAttribute('data-error');
+        input.parentElement.removeAttribute('data-error-visible');
     }
 }
